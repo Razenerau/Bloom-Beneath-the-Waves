2c1bc7846ca45ff2b684b13db7ef8850537b9340
Razenerau <molokov.phil@gmail.com>
2026-02-28 22:40:02 -0500

    Proper animation

--- Assets/PlayerView.cs
+++ Assets/PlayerView.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator Animator;
+    Vector2 moveInput = Vector2.zero;
+
+    public void Move()//InputAction.CallbackContext c)
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            Animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            Animator.SetBool("isMoving", false);
+        }
+
+        if (Mathf.Abs(horizontal) > 0.01f || Mathf.Abs(vertical) > 0.01f) 
+        {
+            Animator.SetFloat("lastInputX", horizontal);
+            Animator.SetFloat("lastInputY", vertical);
+        }
+
+        Animator.SetFloat("inputX", horizontal);
+        Animator.SetFloat("inputY", vertical);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
     }
 }
